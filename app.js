857525b36bfbdaa596884a87a103af4e6fb4097d
Ryan Whitley <apollolm@gmail.com>
2013-07-15 14:47:17 -0700

    More dynamic.  Relative Path stuff.

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -57,7 +58,7 @@ routes['listTables'] = function (req, res) {
     });
 
     query.on('end', function () {
-	res.render('index', { title: 'pGIS Server', list: table_list })
+        res.render('index', {baseURL: req.url, title: 'pGIS Server', list: table_list, breadcrumbs: [{ link: "/services", name: "Home"}] })
         client.end();
     });
 };
@@ -82,7 +83,7 @@ routes['tableDetail'] = function (req, res) {
     });
 
     query.on('end', function () {
-        res.render('table_details', { title: 'pGIS Server', table_details: table_list })
+        res.render('table_details', { baseURL: req.url, title: 'pGIS Server', table_details: table_list, breadcrumbs: [{ link: "/services", name: "Home" }, { link: "", name: req.params.table }] })
         client.end();
     });
 };
@@ -90,18 +91,20 @@ routes['tableDetail'] = function (req, res) {
 
 //Allow for Table Query
 routes['tableQuery'] = function (req, res) {
-    debugger;
     //If the querystring is empty, just show the regular HTML form.
+
     if (JSON.stringify(req.body) != '{}') {
-        console.log("inside");
-        //debugger;
+
+        //Get POST parameters
         var empty = JSON.stringify(req.body);
 
+        //Setup Connection to PG
         var client = new pg.Client(conString);
         client.connect();
 
         var requestGeometry = "";
 
+        //Did user specify to returnGeometry
         if (req.body.returnGeometry && req.body.returnGeometry.toLowerCase() == "yes") {
             //request the geometry
                requestGeometry = " , ST_AsGeoJSON(st_geometryn(lg.geom, 1), 5)::json As geometry "
@@ -115,6 +118,7 @@ routes['tableQuery'] = function (req, res) {
             requestGeometry = " , ST_AsGeoJSON(st_geometryn(lg.geom, 1), 5)::json As geometry "
         }
 
+        //build SQL query
         var sql = "SELECT row_to_json(fc) " +
          "FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features " +
          "FROM (SELECT 'Feature' As type " +
@@ -123,7 +127,7 @@ routes['tableQuery'] = function (req, res) {
          "  FROM " + req.params.table + " As lg " +
          "  INNER JOIN (";
         
-
+        //Add in WHERE clause, if specified
         var mods = "";
         if (req.body.where) {
             mods = " " + req.body.where;
@@ -133,24 +137,28 @@ routes['tableQuery'] = function (req, res) {
             mods = "WHERE " + mods;
         }
 
-        sql += "SELECT gid, district, total_popu FROM bihar) As lp " +
+        //Finish building SQL String
+        sql += "SELECT gid, district, total_popu FROM " + req.params.table + ") As lp " +
         " ON lg.gid = lp.gid " + mods + ") As f )  As fc;"
 
+        //Log the query to the console, for debugging
         console.log("Query: " + sql);
         var query = client.query(sql);
 
+        //Loop thru results
         var results_list = [];
         query.on('row', function (row) {
             results_list.push(row);
         });
 
+        //On last result, decide how to write out results.
         query.on('end', function () {
-            console.log(req.body.format);
             if (!req.body.format) {
-                //if no format, render html
+                //if no format specified, render html
                 res.render('table_query', { title: 'pGIS Server', table: req.params.table, query_results: results_list, format: req.body.format, where: req.body.where, returnGeometry: req.body.returnGeometry });
             }
             else {
+                //Check which format was specified
                 if (req.body.format && req.body.format == "html") {
                     //Render HTML page with results at bottom
                     res.render('table_query', { title: 'pGIS Server', table: req.params.table, query_results: results_list, format: req.body.format, where: req.body.where, returnGeometry: req.body.returnGeometry });
@@ -161,10 +169,12 @@ routes['tableQuery'] = function (req, res) {
                     res.end(JSON.stringify(results_list));
                 }
             }
+            //End PG connection
             client.end();
         });
     }
     else {
+        //Render Query Form without any results.
         res.render('table_query', { title: 'pGIS Server', table: req.params.table })
     }
 };
@@ -174,21 +184,19 @@ routes['tableQuery'] = function (req, res) {
 
 //Define Paths
 //Root Request
-app.get('/', function (req, res){
-	res.render('index', { title: 'pGIS Server' })
-});
+app.get('/', function (req, res) { res.render('index', { title: 'pGIS Server' }) });
 
 //List All Tables
-app.get('/services/', routes['listTables']);
+app.get('/services', routes['listTables']);
 
 //Table Detail
 app.get('/services/:table', routes['tableDetail']);
 
-//Table Query
-app.get('/services/:table/query/', routes['tableQuery']);
+//Table Query - get - display page with default form
+app.get('/services/:table/query', routes['tableQuery']);
 
-//When a Query gets posted
-app.post('/services/:table/query/', routes['tableQuery'])
+//When a Query gets posted - read attributes from post and render results
+app.post('/services/:table/query', routes['tableQuery']);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- readme.txt
+++ readme.txt
@@ -3,4 +3,5 @@ http://www.bearfruit.org/2013/06/21/start-a-new-node-js-express-app-the-right-wa
 
 to run, use node app.js
 when developing, use nodemon app.js (will auto restart when any changes are made)
+install globally: npm install -g nodemon
 
