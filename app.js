8a41e121c98c808f219f7c11536873f830d39a52
Ryan Whitley <rwhitley@spatialdev.com>
2013-09-01 22:42:27 -0700

    dynamic form writing - GP

--- app.js
+++ app.js
@@ -469,7 +469,7 @@ routes['geoprocessing_operation'] = function (req, res) {
                 //No such operation
                 var args = {};
                 args.view = "geoprocessing_operation";
-                args.breadcrumbs = [{ link: "/services", name: "Home" }, { link: "/services/geoprocessing", name: "Geoprocessing Operations" }, { link: "", name: "Geoprocessing Operation" }];
+                args.breadcrumbs = [{ link: "/services", name: "Home" }, { link: "/services/geoprocessing", name: "Geoprocessing Operations" }, { link: "", name: gpOperation.name }];
                 args.errorMessage = "No such operation.";
                 respond(req, res, args);
                 return;
@@ -478,13 +478,20 @@ routes['geoprocessing_operation'] = function (req, res) {
             //Write out page based on dynamic inputs
             args.formfields = [];
             args._input_arguments = [];
+            args._input_values = [];
+            args.description = gpOperation.description;
+            args.breadcrumbs = [{ link: "/services", name: "Home" }, { link: "/services/geoprocessing", name: "Geoprocessing Operations" }, { link: "", name: gpOperation.name }];
+
 
             //See what the inputs are
             //Also see if any of the inputs were provided as args.
             for (var key in gpOperation.inputs) {
                 if (gpOperation.inputs.hasOwnProperty(key)) {
                     args.formfields.push(key);
-                    if (args[key]) { args._input_arguments.push(key) };
+                    if (args[key]) {
+                        args._input_arguments.push(key);
+                        args._input_values.push({ name: key, value: args[key] });
+                    }
                 }
             }
 
@@ -513,7 +520,7 @@ routes['geoprocessing_operation'] = function (req, res) {
                     }
 
                     args.view = "geoprocessing_operation";
-                    args.breadcrumbs = [{ link: "/services", name: "Home" }, { link: "/services/geoprocessing", name: "Geoprocessing Operations" }, { link: "", name: "Geoprocessing Operation" }];
+                    args.breadcrumbs = [{ link: "/services", name: "Home" }, { link: "/services/geoprocessing", name: "Geoprocessing Operations" }, { link: "", name: gpOperation.name }];
                     args.title = "GeoWebServices";
                     args.featureCollection = features;
 
