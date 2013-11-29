f355da96a1fe4d927b37dbcfd05077a6e45e5a35
Ryan Whitley <rwhitley@spatialdev.com>
2013-11-28 20:52:31 -0700

    remove console debug output

--- endpoints/mapnik/index.js
+++ endpoints/mapnik/index.js
@@ -254,7 +254,7 @@ exports.createPGTileQueryRenderer = flow.define(
                         if (err) throw err;
                         map.add_layer(layer);
 
-                        console.log(map.toXML()); // Debug settings
+                        //console.log(map.toXML()); // Debug settings
 
                         map.extent = bbox;
                         var im = new mapnik.Image(map.width, map.height);
