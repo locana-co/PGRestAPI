05763145cae65ebb8cf6a8c73a22b28dcbf5388d
Ryan Whitley <rwhitley@spatialdev.com>
2013-12-15 12:35:45 -0800

    update buffer zonal stats

--- endpoints/geoprocessing/operations/BufferDissolveZonalStats.js
+++ endpoints/geoprocessing/operations/BufferDissolveZonalStats.js
@@ -10,8 +10,8 @@ var pg = require('pg'),
 //2. Buffer Radius
 
 var operation = {};
-var countries = { 'TZA': { name: 'tanzania', srid: '32747' }, 'BGD': { srid: '32645', name: 'bangladesh' }, 'UGA': { srid: '32747', name: 'uganda' }, 'NGA': { name: 'nigeria', srid: '32747' } };
-
+var countries = { 'TZA': { name: 'tanzania', srid: '32736' }, 'BGD': { srid: '32645', name: 'bangladesh' }, 'UGA': { srid: '32635', name: 'uganda' }, 'NGA': { name: 'nigeria', srid: '32632' } };
+//SRIDs from http://www.sumapa.com/crsxpais.cfm
 /* METADATA */
 
 operation.name = "AccessSummary";
@@ -20,71 +20,30 @@ operation.inputs = {};
 
 operation.outputImage = true;
 
-operation.inputs["where_clause"] = { };
+operation.inputs["where_clause"] = {};
 operation.inputs["buffer_distance"] = { value: 0, units: "" }; //how far and what units?
 operation.inputs["country_code"] = []; //Let user specify input country code
 
-//This will execute for cell coverage, Land Use and buffers ~ 33 seconds for all points in bangladesh
-//operation.Query = "with cc_lu as(" +
-//"select " +
-//"  a.coverage, " +
-//"  b.landuse, " +
-//"   (st_dump(st_intersection(a.geom,b.geom))).geom as geom" +
-//"		from bangladesh_coverage a " +
-//"		inner join bangladesh_urbanareas b on " +
-//"		st_intersects(a.geom,b.geom) " +
-//") " +
-//", " +
-//"--add the intersection between cell coverage, land use and buffers " +
-//"cc_lu_5km as " +
-//"( " +
-//"select  " +
-//"            a.coverage, " +
-//"            a.landuse, " +
-//"            b.featuretype, " +
-//"            (st_dump(st_intersection(a.geom,b.geom))).geom as geom " +
-//"from cc_lu a " +
-//"inner join (SELECT ST_Union(ST_transform( ST_BUFFER( ST_transform(geom, 32645), 5000 ), 4326 )) as geom, featuretype " +
-//"FROM bangladesh_cicos " +
-//"WHERE featuretype = 'Post Offices' GROUP BY featuretype) b on " +
-//"st_intersects(a.geom, b.geom) " +
-//")" +
-//"select coverage, landuse, featuretype, ST_AsGeoJSON(geom) as geom from cc_lu_5km;";
-
-
-//This will execute just for Land Use and buffers - ~ 18 seconds for all points in bangladesh
-//operation.Query = "with cc_lu_5km as " +
-//"( " +
-//"select  " +
-//"            a.landuse, " +
-//"            b.featuretype, " +
-//"            (st_dump(st_intersection(a.geom,b.geom))).geom as geom " +
-//"from {country}_urbanareas a " +
-//"inner join (SELECT ST_Union(ST_transform( ST_BUFFER( ST_transform(geom, {srid}), 5000 ), 4326 )) as geom, featuretype " +
-//"FROM {country}_cicos " +
-//"WHERE {where_clause} GROUP BY featuretype) b on " +
-//"st_intersects(a.geom, b.geom) " +
-//")" +
-//"select landuse, featuretype, ST_AsGeoJSON(geom) as geom from cc_lu_5km;"
-
-
-//This will execute just for Land Use and buffers - ~ 18 seconds for all points in bangladesh
+//This will execute just for Land Use and buffers - ~ 4 seconds for all points in Uganda, minus rural
 operation.Query = "DO $$DECLARE " +
-"orig_srid int; " +
 "BEGIN " +
 "drop table if exists _gptemp; " +
-"create temporary table _gptemp as " +
-"select  " +
-"a.landuse, " +
-"b.featuretype, " +
-"ST_AsGeoJSON((st_dump(st_intersection(a.geom,b.geom))).geom) as geom " +
+"create temporary  table _gptemp as  " +
+"select a.landuse, " +
+"ST_UNION(st_intersection(a.geom,b.geom)) as geom " +
 "from {country}_urbanareas a " +
-"inner join (SELECT ST_Union(ST_transform( ST_BUFFER( ST_transform(geom, {srid}), 5000 ), 4326 )) as geom, featuretype " +
+"inner join (SELECT ST_Union(ST_transform( ST_BUFFER( ST_transform(geom, {srid}), {buffer_distance}), 4326 )) as geom " +
 "FROM {country}_cicos " +
-"WHERE {where_clause} GROUP BY featuretype) b on " +
-"st_intersects(a.geom, b.geom); " +
+"WHERE {where_clause} " +
+") b on " +
+"st_intersects(a.geom, b.geom) " +
+"GROUP BY a.landuse; " +
+"CREATE INDEX _gptemp_gix ON _gptemp USING GIST (geom); " +
 "END$$; " +
-"select * from _gptemp;"
+"SELECT SUM((_st_summarystats(ST_Clip(rast,_gptemp.geom, true), 1, true, .99)).sum) as  sum, _gptemp.landuse, ST_AsGeoJSON(_gptemp.geom) as geom " +
+"FROM uganda_population_raster, _gptemp " +
+"WHERE ST_Intersects(_gptemp.geom,rast) " +
+"GROUP BY _gptemp.landuse, _gptemp.geom; ";
 
 
 
@@ -93,21 +52,22 @@ operation.execute = flow.define(
         this.args = args;
         this.callback = callback;
         //Step 1
+        debugger;
         //See if inputs are set. Incoming arguments should contain the same properties as the input parameters.
         if (operation.isInputValid(args) === true) {
             operation.inputs["where_clause"] = args.where_clause;
             operation.inputs["buffer_distance"] = args.buffer_distance;
-            operation.inputs["country_code"] = args.country_code;
+            operation.inputs["country_code"] = args.country_code.toUpperCase();
 
             //Take the point and buffer it in PostGIS
-            var query = { text: operation.Query.replace("{where_clause}", operation.inputs["where_clause"]).split("{country}").join(countries[operation.inputs["country_code"]].name).replace("{srid}", countries[operation.inputs["country_code"]].srid), values: []};
+            var query = { text: operation.Query.replace("{where_clause}", operation.inputs["where_clause"]).replace("{buffer_distance}", operation.inputs["buffer_distance"]).split("{country}").join(countries[operation.inputs["country_code"]].name).replace("{srid}", countries[operation.inputs["country_code"]].srid), values: [] };
             common.executePgQuery(query, this);//Flow to next function when done.
 
         }
         else {
             //Invalid arguments
             //return message
-            callback({ status: "Missing required arguments", rows: []});
+            callback({ status: "Missing required arguments", rows: [] });
         }
     },
     function (results) {
@@ -123,7 +83,7 @@ operation.isInputValid = function (input) {
 
     if (input) {
         //make sure we have a where clause, buffer disatance and the country code is found in the country object.
-        if (input["where_clause"] && input["buffer_distance"] && countries[input["country_code"]]) {
+        if (input["where_clause"] && input["buffer_distance"] && countries[input["country_code"].toUpperCase()]) {
             //It's got everything we need.
             return true;
         }
