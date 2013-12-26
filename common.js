3c69cc1d776fea78942c9e145e7301e29448960e
Ryan Whitley <rwhitley@spatialdev.com>
2013-12-26 11:55:20 -0800

    clickable header, calc response time for html only

--- common.js
+++ common.js
@@ -7,12 +7,12 @@ var common = {};
 common.formatters = {};
 
 common.respond = function (req, res, args) {
-    
-    //calculate response time
-    args.responseTime = new Date - req._startTime; //ms since start of request
-
+   
     //Write out a response as JSON or HTML with the appropriate arguments.  Add more formats here if desired
     if (!args.format || args.format.toLowerCase() == "html") {
+        //calculate response time
+        args.responseTime = new Date - req._startTime; //ms since start of request
+
         //Determine sample request based on args
         res.render(args.view, args);
     }
