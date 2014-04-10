5436043853b248cdcf52e9841dd934c85f63e9b5
Ryan Whitley <rwhitley@spatialdev.com>
2014-04-10 13:59:58 -0700

    don't freak out if tilestream isn't present.

--- endpoints/tiles/index.js
+++ endpoints/tiles/index.js
@@ -14,72 +14,85 @@ exports.app = function(passport) {
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 
-	//If tilestream endpoint is provided
-	if (settings.tilestream && settings.tilestream.host && settings.tilestream.path) {
+
 		//List All TileStream
 		app.all('/services/tiles', function(req, res) {
 
-			var args = {};
 
-			//Grab POST or QueryString args depending on type
-			if (req.method.toLowerCase() == "post") {
-				//If a post, then arguments will be members of the this.req.body property
-				args = req.body;
-			} else if (req.method.toLowerCase() == "get") {
-				//If request is a get, then args will be members of the this.req.query property
-				args = req.query;
-			}
+			//If tilestream endpoint is provided
+			if (settings.tilestream && settings.tilestream.host && settings.tilestream.path) {
+				var args = {};
 
-			args.view = "tile_list";
-			args.breadcrumbs = [{
-				link : "/services",
-				name : "Table Listing"
-			}, {
-				link : "",
-				name : "Tile List"
-			}];
-			args.path = req.path;
-			args.host = req.headers.host;
+				//Grab POST or QueryString args depending on type
+				if (req.method.toLowerCase() == "post") {
+					//If a post, then arguments will be members of the this.req.body property
+					args = req.body;
+				} else if (req.method.toLowerCase() == "get") {
+					//If request is a get, then args will be members of the this.req.query property
+					args = req.query;
+				}
 
-			try {
-				var options = {
-					host : settings.tilestream.host,
-					path : settings.tilestream.path
-				};
+				args.view = "tile_list";
+				args.breadcrumbs = [
+					{
+						link: "/services",
+						name: "Table Listing"
+					},
+					{
+						link: "",
+						name: "Tile List"
+					}
+				];
+				args.path = req.path;
+				args.host = req.headers.host;
 
-				//add port if specified
-				if (settings.tilestream.port)
-					options.port = settings.tilestream.port;
+				try {
+					var options = {
+						host: settings.tilestream.host,
+						path: settings.tilestream.path
+					};
 
-				http.request(options, function(response) {
-					var str = '';
+					//add port if specified
+					if (settings.tilestream.port)
+						options.port = settings.tilestream.port;
 
-					//another chunk of data has been recieved, so append it to `str`
-					response.on('data', function(chunk) {
-						str += chunk;
-					});
+					http.request(options, function (response) {
+						var str = '';
 
-					//the whole response has been recieved, so we just print it out here
-					response.on('end', function() {
-						var obj = JSON.parse(str);
-						//This is an array of tilestream layers.  break it apart so it's easier to parse for the UI
-						args.featureCollection = [];
-						obj.forEach(function(layer) {
-							args.featureCollection.push({
-								name : layer.name,
-								preview : "http://" + settings.tilestream.host + ":" + (settings.tilestream.port ? settings.tilestream.port : 80) + "/#!/map/" + layer.name
-							});
+						//another chunk of data has been recieved, so append it to `str`
+						response.on('data', function (chunk) {
+							str += chunk;
 						});
 
-						common.respond(req, res, args);
+						//the whole response has been recieved, so we just print it out here
+						response.on('end', function () {
+							var obj = JSON.parse(str);
+							//This is an array of tilestream layers.  break it apart so it's easier to parse for the UI
+							args.featureCollection = [];
+							obj.forEach(function (layer) {
+								args.featureCollection.push({
+									name: layer.name,
+									preview: "http://" + settings.tilestream.host + ":" + (settings.tilestream.port ? settings.tilestream.port : 80) + "/#!/map/" + layer.name
+								});
+							});
 
-					});
-				}).end();
+							common.respond(req, res, args);
 
-			} catch (e) {
-				common.respond(req, res, args);
+						});
+					}).end();
+
+				} catch (e) {
+					common.respond(req, res, args);
+				}
+			}else{
+					//Just report how they can register a tilestream endpoint
+				  res.end("No tilestream URL registered.  Add it to settings to see a tilestream endpoint here.");
 			}
+
+
 		});
-	}
+
+
+
 	return app;
 }
