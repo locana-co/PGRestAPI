ae48f238505105a7965095dbc197408d7c7d4925
Ryan Whitley <apollolm@gmail.com>
2014-10-29 14:27:22 -0700

    Actual pcode code.

--- endpoints/geoprocessing/operations/GetPCodeStackByXY.js
+++ endpoints/geoprocessing/operations/GetPCodeStackByXY.js
@@ -5,103 +5,47 @@
 var flow = require('flow');
 var pg = require('pg');
 var common = require("../../../common");
-var GeoFragger = require('../../../lib/GeoFragger');
-var geoFragger = new GeoFragger();
 
-//Takes in a POINT (X Y) and returns a WKT representation of the buffer.
+//Takes in a POINT (X Y) and returns JSON object containing the intersected boundaries along with pcodes if applicable.
 //Arguments are:
-//1. The POINT
-//2. Buffer Radius
+//1. X (Longitude)
+//2. Y (Latitude)
 
-var Buffer = {};
+
+var operation = {};
 
 
 /* METADATA */
 
-Buffer.name = "Buffer";
-Buffer.description = "Buffers a point and returns the geometry as WKT. `buffer_distance` is the radius of the buffer in meters. `input_format` can be either WKT or GeoJSON.";
-Buffer.inputs = {};
-
-Buffer.inputs["input_geometry"] = { wkt: "", srid: ""};
-Buffer.inputs["buffer_distance"] = { value: 0, units: ""}; //how far and what units?
-Buffer.inputs["input_format"] = '';
+operation.name = "GetPCodeStackByXY";
+operation.description = "Takes an X and Y coordinate, returns JSON object with intersected GADM boundaries with any pcodes that are available.";
+operation.inputs = {};
+operation.geom_columns = ['geom0', 'geom1', 'geom2', 'geom3', 'geom4', 'geom5']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
 
 
-//Since we don't want to be writing functions, use this anonymous code block in postgres.
-var wktBufferQuery = "DO $$DECLARE " +
-  "orig_srid int; " +
-  "utm_srid int; " +
-  "input geometry := ST_GeomFromText('{wkt}', 4326); " +
-  "geomgeog geometry; " +
-  "zone int; " +
-  "pref int; " +
-  "BEGIN " +
-  "geomgeog:= ST_Transform(input,4326); " +
-  "IF (ST_Y(geomgeog))>0 THEN " +
-  "pref:=32600; " +
-  "ELSE " +
-  "pref:=32700; " +
-  "END IF; " +
-  "zone:=floor((ST_X(geomgeog)+180)/6)+1; " +
-  "orig_srid:= ST_SRID(input); " +
-  "utm_srid:= zone+pref; " +
-  "drop table if exists _temp; " +
-  "create temporary table _temp as " +
-  "SELECT ST_AsGeoJSON(ST_transform(ST_Buffer(ST_transform(input, utm_srid), {buffer_distance}), orig_srid)) as geom; " +
-  "END$$; " +
-  "select * from _temp;";
+operation.inputs["x"] = { };
+operation.inputs["y"] = { };
 
-var geoJsonBufferQuery = "DO $$DECLARE " +
-  "orig_srid int; " +
-  "utm_srid int; " +
-  "input geometry := ST_GeomFromGeoJSON('{geojson}'); " +
-  "geomgeog geometry; " +
-  "zone int; " +
-  "pref int; " +
-  "BEGIN " +
-  "geomgeog:= ST_Transform(input,4326); " +
-  "IF (ST_Y(geomgeog))>0 THEN " +
-  "pref:=32600; " +
-  "ELSE " +
-  "pref:=32700; " +
-  "END IF; " +
-  "zone:=floor((ST_X(geomgeog)+180)/6)+1; " +
-  "orig_srid:= ST_SRID(input); " +
-  "utm_srid:= zone+pref; " +
-  "drop table if exists _temp; " +
-  "create temporary table _temp as " +
-  "SELECT ST_AsGeoJSON(ST_transform(ST_Buffer(ST_transform(input, utm_srid), {buffer_distance}), orig_srid)) as geom; " +
-  "END$$; " +
-  "select * from _temp;";
-
-
-Buffer.execute = flow.define(
+operation.execute = flow.define(
   function(args, callback) {
+
+    //The query to be executed
+
+    //This contains Geom, but doesn't output in the HTML correctly.
+    //var query = "select name0,guid0,ST_AsGeoJSON(geom0) as geom0,pcode0,name1,guid1,ST_AsGeoJSON(geom1) as geom1,pcode1,name2,guid2,ST_AsGeoJSON(geom2) as geom2,pcode2,name3,guid3,ST_AsGeoJSON(geom3) as geom3,pcode3,name4,guid4,ST_AsGeoJSON(geom4) as geom4,pcode4,name5,guid5,ST_AsGeoJSON(geom5) as geom5,pcode5  from gadmrollup where ST_Intersects(ST_GeomFromText('POINT({{x}} {{y}})', 4326), geom3);";
+    var query = "select name0,guid0,pcode0,name1,guid1,pcode1,name2,guid2,pcode2,name3,guid3,pcode3,name4,guid4,pcode4,name5,guid5,pcode5  from gadmrollup where ST_Intersects(ST_GeomFromText('POINT({{x}} {{y}})', 4326), geom3);";
+
+
     this.args = args;
     this.callback = callback;
     //Step 1
 
     //See if inputs are set. Incoming arguments should contain the same properties as the input parameters.
-    if (Buffer.isInputValid(args) === true) {
-      var inputGeometry = Buffer.inputs["input_geometry"] = args.input_geometry;
-      var bufferDistance = Buffer.inputs["buffer_distance"] = args.buffer_distance;
-      var inputFormat = Buffer.inputs["input_format"] = args.input_format;
+    if (operation.isInputValid(args) === true) {
+      var x = operation.inputs["x"] = args.x;
+      var y = operation.inputs["y"] = args.y;
 
-      //Take the point and buffer it in PostGIS
-
-      if (inputFormat.toLowerCase() === 'wkt') {
-        var sql = wktBufferQuery
-          .replace("{wkt}", inputGeometry)
-          .replace("{buffer_distance}", bufferDistance);
-      }
-
-      else if (inputFormat.toLowerCase() === 'geojson') {
-        var geoJson = geoFragger.toPostGISFragment(JSON.parse(inputGeometry));
-//        var geoJson = '{"type": "Point", "coordinates": [77.24212646484374,28.586933499067946],"crs":{"type":"name","properties":{"name":"EPSG:4326"}}}';
-        var sql = geoJsonBufferQuery
-          .replace("{geojson}", JSON.stringify(geoJson))
-          .replace("{buffer_distance}", bufferDistance);
-      }
+      var sql = { text: query.replace("{{x}}", x).replace("{{y}}", y), values: []};
 
       common.executePgQuery(sql, this);//Flow to next function when done.
 
@@ -109,7 +53,7 @@ Buffer.execute = flow.define(
     else {
       //Invalid arguments
       //return message
-      callback({text: "Missing required arguments"}); //err is first argument
+      callback({text: "Missing required arguments: x, y"});
     }
   },
   function(err, results) {
@@ -119,11 +63,10 @@ Buffer.execute = flow.define(
 );
 
 //Make sure arguments are tight before executing
-Buffer.isInputValid = function(input) {
+operation.isInputValid = function(input) {
 
   if (input) {
-    if (input["input_geometry"] && input["buffer_distance"] &&
-          input["input_format"] && (input["input_format"].toLowerCase() === 'wkt' || input["input_format"].toLowerCase() === 'geojson')) {
+    if (input["x"] && input["y"]) {
       //It's got everything we need.
       return true;
     }
@@ -132,4 +75,4 @@ Buffer.isInputValid = function(input) {
   return false;
 };
 
-module.exports = Buffer;
+module.exports = operation;
