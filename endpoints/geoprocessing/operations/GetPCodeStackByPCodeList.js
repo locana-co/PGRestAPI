c3b2b1e791929c94d72fc8afbe5edcc2887e913d
Ryan Whitley <apollolm@gmail.com>
2014-11-13 20:01:30 -0800

    Get pcodes by comma delimited pcode list

--- endpoints/geoprocessing/operations/GetPCodeStackByPCodeList.js
+++ endpoints/geoprocessing/operations/GetPCodeStackByPCodeList.js
@@ -18,7 +18,7 @@ operation.inputs = {};
 
 
 operation.inputs["list"] = { value: "", required: true, help: "p-code list code1,code2,code3,code4" };
-operation.inputs["subDistricts"] = { value: "", required: false, default_value: false,  help: "boolean - return full stack?" };
+//operation.inputs["subDistricts"] = { value: "", required: false, default_value: false,  help: "boolean - return full stack?" };
 operation.inputs["geometryType"] = { value: "", required: false, default_value: 'polygon', help: "point or polygon"};
 
 operation.execute = flow.define(
@@ -29,7 +29,7 @@ operation.execute = flow.define(
     //Step 1
 
     var list = operation.inputs["list"].value = args.list;
-    var subDistricts = operation.inputs["subDistricts"].value = (args.subDistricts || operation.inputs["subDistricts"].default_value);
+    //var subDistricts = operation.inputs["subDistricts"].value = (args.subDistricts || operation.inputs["subDistricts"].default_value);
     var geometryType = operation.inputs["geometryType"].value = (args.geometryType || operation.inputs["geometryType"].default_value);
 
 
@@ -43,86 +43,71 @@ operation.execute = flow.define(
         return item.trim();
       });
 
-      var pCodeAdminObject = {};
 
-      //Now loop and see what admin levels we're dealing with
-      pcodeList.forEach(function(item){
+      //Query DB for each item in the list
+      pcodeList.forEach(function(pcode){
 
-        //See how many characters we have.
-        //if length > 3, assume we're not at a level 0, which has either 2 or 3 characters
-        if(item){
-          if(item.length > 3){
-            //Start stripping values off of the end of the string 3 at a time (sub regions below level 0 are 3 digit codes.
-            var adminLevel = snipPCodes(item).length - 1;
-            pCodeAdminObject[item] = { level: adminLevel };
 
-          }else if(item.length == 2 || item.length == 3){
-            //Assume a Level 0
-            pCodeAdminObject[item] = { level: 0}
-          }
+        var query0, query1, query2, query3;
+
+
+        //No subdistricts - exact matches only
+        //if(subDistricts == false){
+        //Find the exact match by using a UNION all thru each level.
+        query3 = "select DISTINCT ON (pcode{{level}}) 3 as level, name{{level}} as name,guid{{level}} as guid, pcode{{level}} as pcode {{geometry}} from gadmrollup where pcode{{level}} = {{list}}".split("{{level}}").join("3");
+        query2 = "UNION ALL select DISTINCT ON (pcode{{level}}) 2 as level, name{{level}},guid{{level}},pcode{{level}} {{geometry}} from gadmrollup where pcode{{level}} = {{list}}".split("{{level}}").join("2");
+        query1 = "UNION ALL select DISTINCT ON (pcode{{level}}) 1 as level, name{{level}},guid{{level}},pcode{{level}} {{geometry}} from gadmrollup where pcode{{level}} = {{list}}".split("{{level}}").join("1");
+        query0 = "UNION ALL select DISTINCT ON (pcode{{level}}) 0 as level, name{{level}},guid{{level}},pcode{{level}} {{geometry}} from gadmrollup where pcode{{level}} = {{list}};".split("{{level}}").join("0");
+
+        if(geometryType == 'polygon'){
+          //Geometry, replace the geometry placeholder with the geometry columns.
+          query3 = query3.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geom').split("{{level}}").join("3");
+          query2 = query2.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geom').split("{{level}}").join("2");
+          query1 = query1.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geom').split("{{level}}").join("1");
+          query0 = query0.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geom').split("{{level}}").join("0");
+
+          //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+          operation.geom_columns = ['geom']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+
+        }else{
+          //Return the centroid (point) geometry
+          //Geometry, replace the geometry placeholder with the geometry columns.
+          query3 = query3.split('{{geometry}}').join(', ST_AsGeoJSON(ST_Centroid(geom{{level}})) as geom_centroid').split("{{level}}").join("3");
+          query2 = query2.split('{{geometry}}').join(', ST_AsGeoJSON(ST_Centroid(geom{{level}})) as geom_centroid').split("{{level}}").join("2");
+          query1 = query1.split('{{geometry}}').join(', ST_AsGeoJSON(ST_Centroid(geom{{level}})) as geom_centroid').split("{{level}}").join("1");
+          query0 = query0.split('{{geometry}}').join(', ST_AsGeoJSON(ST_Centroid(geom{{level}})) as geom_centroid').split("{{level}}").join("0");
+
+          //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+          operation.geom_columns = ['geom_centroid']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
         }
 
-      });
+        //}
+        //else{
+        //  query0 = "select name0,guid0,pcode0,name1,guid1,pcode1,name2,guid2,pcode2,name3,guid3,pcode3,name4,guid4,pcode4,name5,guid5,pcode5 {{geometry}} from gadmrollup where pcode{{level}} = {{list}};";
+        //
+        //  if(geometryType == 'polygon'){
+        //    //Geometry, replace the geometry placeholder with the geometry columns.
+        //    query0 = query0.replace('{{geometry}}', ', ST_AsGeoJSON(geom0) as geom0,ST_AsGeoJSON(geom1) as geom1,ST_AsGeoJSON(geom2) as geom2,ST_AsGeoJSON(geom3) as geom3,ST_AsGeoJSON(geom4) as geom4,ST_AsGeoJSON(geom5) as geom5');
+        //    //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+        //    operation.geom_columns = ['geom0', 'geom1', 'geom2', 'geom3', 'geom4', 'geom5']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+        //
+        //  }else{
+        //    //Return the centroid (point) geometry
+        //    //Geometry, replace the geometry placeholder with the geometry columns.
+        //    query0 = query0.replace('{{geometry}}', ', ST_AsGeoJSON(ST_Centroid(geom0)) as geom0_centroid,ST_AsGeoJSON(ST_Centroid(geom1)) as geom1_centroid,ST_AsGeoJSON(ST_Centroid(geom2)) as geom2_centroid,ST_AsGeoJSON(ST_Centroid(geom3)) as geom3_centroid,ST_AsGeoJSON(ST_Centroid(geom4)) as geom4_centroid,ST_AsGeoJSON(ST_Centroid(geom5)) as geom5_centroid');
+        //    //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+        //    operation.geom_columns = ['geom0_centroid', 'geom1_centroid', 'geom2_centroid', 'geom3_centroid', 'geom4_centroid', 'geom5_centroid']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+        //  }
+        //}
 
-      //The SQL Query to be executed;
+        var query = [query3, query2, query1, query0].join(" ")
 
+        //Wrap incoming pcodes with single quotes for the SQL IN clause
+        var sql = { text: query.split("{{list}}").join("'" + pcode + "'"), values: []};
 
-      for (var key in pCodeAdminObject){
-        if (pCodeAdminObject.hasOwnProperty(key)) {
+        common.executePgQuery(sql, this.MULTI(key));//Flow to next function when ALL are done.
 
-          var query;
-          var level = pCodeAdminObject[key].level;
-
-          if(subDistricts == false){
-            query = "select DISTINCT ON (pcode{{level}}) name{{level}},guid{{level}},pcode{{level}} {{geometry}} from gadmrollup where pcode{{level}} = {{list}};";
-
-            if(geometryType == 'polygon'){
-              //Geometry, replace the geometry placeholder with the geometry columns.
-              query = query.replace('{{geometry}}', ', ST_AsGeoJSON(geom{{level}}) as geom{{level}}');
-              //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-              operation.geom_columns = ['geom' + level]; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-
-            }else{
-              //Return the centroid (point) geometry
-              //Geometry, replace the geometry placeholder with the geometry columns.
-              query = query.replace('{{geometry}}', ', ST_AsGeoJSON(ST_Centroid(geom{{level}})) as geom{{level}}_centroid');
-              //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-              operation.geom_columns = ['geom' + level + '_centroid']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-            }
-
-            query = query.split("{{level}}").join(level);
-
-          }
-          else{
-            query = "select name0,guid0,pcode0,name1,guid1,pcode1,name2,guid2,pcode2,name3,guid3,pcode3,name4,guid4,pcode4,name5,guid5,pcode5 {{geometry}} from gadmrollup where pcode{{level}} = {{list}};";
-
-            if(geometryType == 'polygon'){
-              //Geometry, replace the geometry placeholder with the geometry columns.
-              query = query.replace('{{geometry}}', ', ST_AsGeoJSON(geom0) as geom0,ST_AsGeoJSON(geom1) as geom1,ST_AsGeoJSON(geom2) as geom2,ST_AsGeoJSON(geom3) as geom3,ST_AsGeoJSON(geom4) as geom4,ST_AsGeoJSON(geom5) as geom5');
-              //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-              operation.geom_columns = ['geom0', 'geom1', 'geom2', 'geom3', 'geom4', 'geom5']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-
-            }else{
-              //Return the centroid (point) geometry
-              //Geometry, replace the geometry placeholder with the geometry columns.
-              query = query.replace('{{geometry}}', ', ST_AsGeoJSON(ST_Centroid(geom0)) as geom0_centroid,ST_AsGeoJSON(ST_Centroid(geom1)) as geom1_centroid,ST_AsGeoJSON(ST_Centroid(geom2)) as geom2_centroid,ST_AsGeoJSON(ST_Centroid(geom3)) as geom3_centroid,ST_AsGeoJSON(ST_Centroid(geom4)) as geom4_centroid,ST_AsGeoJSON(ST_Centroid(geom5)) as geom5_centroid');
-              //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-              operation.geom_columns = ['geom0_centroid', 'geom1_centroid', 'geom2_centroid', 'geom3_centroid', 'geom4_centroid', 'geom5_centroid']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-            }
-
-            query = query.split("{{level}}").join(level);
-
-          }
-
-          //Wrap incoming pcodes with single quotes for the SQL IN clause
-          var sql = { text: query.split("{{list}}").join("'" + key + "'"), values: []};
-
-          common.executePgQuery(sql, this.MULTI(key));//Flow to next function when ALL are done.
-        }
-      }
-
-
-
+      }, this)
 
     }
     else {
@@ -160,6 +145,11 @@ operation.isInputValid = function(input) {
   return true;
 };
 
+
+/**
+ *
+ * No Longer Used.  Only works for 'GINnnnxxx' formatted 3 digit codes
+ * **/
 //Take in a string, chop off the last 3 characters.
 //Return the chopped portion of the string, plus the leftovers.
 function snipPCodes(input){
