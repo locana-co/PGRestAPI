d326c17420f8b8aebd229f9ac15e81b5f10870bc
Ryan Whitley <apollolm@gmail.com>
2014-11-17 09:29:34 -0800

    Working on pcode endpoint. Updated common csv formatter to try to recognize the integer 0 as a value.

--- common.js
+++ common.js
@@ -511,7 +511,7 @@ common.formatters.CSVFormatter = function (rows, geom_fields_array) {
 
         for (var index in row) {
             if (geom_fields_array.indexOf(index) == -1)
-                csvArray.push((row[index] || '') + ",");
+                csvArray.push((row[index] || (row[index] == 0 ? row[index] : '')) + ",");
         }
         //Add newline
         csvArray.push('\r\n');
--- endpoints/geoprocessing/operations/GetPCodeStackByPCodeList.js
+++ endpoints/geoprocessing/operations/GetPCodeStackByPCodeList.js
@@ -26,7 +26,9 @@ operation.execute = flow.define(
 
     this.args = args;
     this.callback = callback;
-    //Step 1
+
+
+    operation.geom_columns = []; //reset operation geom_columns for output formatting
 
     var list = operation.inputs["list"].value = args.list;
     //var subDistricts = operation.inputs["subDistricts"].value = (args.subDistricts || operation.inputs["subDistricts"].default_value);
@@ -57,17 +59,17 @@ operation.execute = flow.define(
         query3 = "select DISTINCT ON (pcode{{level}}) 3 as level, name{{level}} as name,guid{{level}} as guid, pcode{{level}} as pcode {{geometry}} from gadmrollup where pcode{{level}} = {{list}}".split("{{level}}").join("3");
         query2 = "UNION ALL select DISTINCT ON (pcode{{level}}) 2 as level, name{{level}},guid{{level}},pcode{{level}} {{geometry}} from gadmrollup where pcode{{level}} = {{list}}".split("{{level}}").join("2");
         query1 = "UNION ALL select DISTINCT ON (pcode{{level}}) 1 as level, name{{level}},guid{{level}},pcode{{level}} {{geometry}} from gadmrollup where pcode{{level}} = {{list}}".split("{{level}}").join("1");
-        query0 = "UNION ALL select DISTINCT ON (pcode{{level}}) 0 as level, name{{level}},guid{{level}},pcode{{level}} {{geometry}} from gadmrollup where pcode{{level}} = {{list}};".split("{{level}}").join("0");
+        query0 = "UNION ALL select DISTINCT ON (pcode{{level}}) '0' as level, name{{level}},guid{{level}},pcode{{level}} {{geometry}} from gadmrollup where pcode{{level}} = {{list}};".split("{{level}}").join("0");
 
         if(geometryType == 'polygon'){
           //Geometry, replace the geometry placeholder with the geometry columns.
-          query3 = query3.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geom').split("{{level}}").join("3");
-          query2 = query2.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geom').split("{{level}}").join("2");
-          query1 = query1.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geom').split("{{level}}").join("1");
-          query0 = query0.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geom').split("{{level}}").join("0");
+          query3 = query3.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geometry').split("{{level}}").join("3");
+          query2 = query2.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geometry').split("{{level}}").join("2");
+          query1 = query1.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geometry').split("{{level}}").join("1");
+          query0 = query0.split('{{geometry}}').join(', ST_AsGeoJSON(geom{{level}}) as geometry').split("{{level}}").join("0");
 
           //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-          operation.geom_columns = ['geom']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+          if (this.args.format != "csv") operation.geom_columns = ['geometry']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
 
         }else{
           //Return the centroid (point) geometry
@@ -78,27 +80,8 @@ operation.execute = flow.define(
           query0 = query0.split('{{geometry}}').join(', ST_AsGeoJSON(ST_Centroid(geom{{level}})) as geom_centroid').split("{{level}}").join("0");
 
           //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-          operation.geom_columns = ['geom_centroid']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
+          if (this.args.format != "csv") operation.geom_columns = ['geom_centroid']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
         }
-
-        //}
-        //else{
-        //  query0 = "select name0,guid0,pcode0,name1,guid1,pcode1,name2,guid2,pcode2,name3,guid3,pcode3,name4,guid4,pcode4,name5,guid5,pcode5 {{geometry}} from gadmrollup where pcode{{level}} = {{list}};";
-        //
-        //  if(geometryType == 'polygon'){
-        //    //Geometry, replace the geometry placeholder with the geometry columns.
-        //    query0 = query0.replace('{{geometry}}', ', ST_AsGeoJSON(geom0) as geom0,ST_AsGeoJSON(geom1) as geom1,ST_AsGeoJSON(geom2) as geom2,ST_AsGeoJSON(geom3) as geom3,ST_AsGeoJSON(geom4) as geom4,ST_AsGeoJSON(geom5) as geom5');
-        //    //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-        //    operation.geom_columns = ['geom0', 'geom1', 'geom2', 'geom3', 'geom4', 'geom5']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-        //
-        //  }else{
-        //    //Return the centroid (point) geometry
-        //    //Geometry, replace the geometry placeholder with the geometry columns.
-        //    query0 = query0.replace('{{geometry}}', ', ST_AsGeoJSON(ST_Centroid(geom0)) as geom0_centroid,ST_AsGeoJSON(ST_Centroid(geom1)) as geom1_centroid,ST_AsGeoJSON(ST_Centroid(geom2)) as geom2_centroid,ST_AsGeoJSON(ST_Centroid(geom3)) as geom3_centroid,ST_AsGeoJSON(ST_Centroid(geom4)) as geom4_centroid,ST_AsGeoJSON(ST_Centroid(geom5)) as geom5_centroid');
-        //    //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-        //    operation.geom_columns = ['geom0_centroid', 'geom1_centroid', 'geom2_centroid', 'geom3_centroid', 'geom4_centroid', 'geom5_centroid']; //List the expected geom columns that come back from this query, so the formatters know which geoms to convert to GeoJSON
-        //  }
-        //}
 
         var query = [query3, query2, query1, query0].join(" ")
 
@@ -121,8 +104,23 @@ operation.execute = flow.define(
     var mergedResults = {rows: []};
 
     results.forEach(function(result){
+
+      //Go thru each row.
+      if(result[1].rows){
+        result[1].rows.forEach(function(row){
+          //If csv, then escape the geometry and geom_centroid columns
+          if (this.args.format == "csv") {
+            //Add double quotes around the geometry column, and replace double quotes inside with 2 double quotes to escape it for CSV.
+            if(row["geometry"]) row["geometry"] = '"' + row["geometry"].split('"').join('""') + '"';
+            if(row["geom_centroid"]) row["geom_centroid"] = '"' + row["geom_centroid"].split('"').join('""') + '"';
+          }
+        }, this)
+      }
+
+
+      //Merge the results in with any previous results
       mergedResults.rows = mergedResults.rows.concat(result[1].rows);
-    });
+    }, this);
 
     this.callback(null, mergedResults);
   }
